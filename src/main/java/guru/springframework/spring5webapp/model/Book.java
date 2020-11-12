ad6fa83c3ceb29b856617144c968f395972d7602
Jerome Moussalli <jerome.moussalli@gmail.com>
2020-11-12 17:12:37 +0100

    Entities Author & Book JPA Relationships Annotations

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring5webapp.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.sun.tools.javac.comp.Resolve;
+
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -15,6 +14,10 @@ public class Book {
 
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
