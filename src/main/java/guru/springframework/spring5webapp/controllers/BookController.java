13a493599289a14c5788500593308035eee67484
Jerome Moussalli <jerome.moussalli@gmail.com>
2020-11-14 16:16:37 +0100

    Début Controllers (BookController)

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Package :  guru.springframework.spring5webapp.controllers
+ * Created by jerom on 14/11/2020 at 16:09
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("Books", bookRepository.findAll());
+
+        return "Books";
+    }
+}
